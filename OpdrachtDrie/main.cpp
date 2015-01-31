db9770d80e999db4fc8d5543cd9e8fbdfc5691b1
JeroenElschot <jeroen_elschot@hotmail.com>
2015-01-31 13:51:15 +0100

    wiki number reeks

--- OpdrachtDrie/main.cpp
+++ OpdrachtDrie/main.cpp
@@ -11,7 +11,8 @@ int main()
     BinaryTree<int> *bin = new BinaryTree<int>();
 
    // int numberlist [5] = { 1,8, 3, 4, 10 };
-    int numberlist [9] = { 2,1,3,7,10,5,8,6,4 };
+    //int numberlist [9] = { 2,1,3,7,10,5,8,6,4 };
+    int numberlist [9] = { 8,3,1,6,4,7,10,14,13 }; // http://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Binary_search_tree.svg/220px-Binary_search_tree.svg.png
     //int numberlist [15] = { 1,8, 3, 4, 10,11,14,29,21,2,5,6,7,30,31 };
     for ( int n=0 ; n<9 ; ++n )
     {
