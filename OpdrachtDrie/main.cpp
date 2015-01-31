2506208dda0821641ed14187361c943aa916ef2c
Richard Olthuis <rolthuis.mail@gmail.com>
2015-01-31 13:49:10 +0100

    updated form

--- OpdrachtDrie/main.cpp
+++ OpdrachtDrie/main.cpp
@@ -10,9 +10,7 @@ int main()
 {
     BinaryTree<int> *bin = new BinaryTree<int>();
 
-   // int numberlist [5] = { 1,8, 3, 4, 10 };
-    int numberlist [9] = { 2,1,3,7,10,5,8,6,4 };
-    //int numberlist [15] = { 1,8, 3, 4, 10,11,14,29,21,2,5,6,7,30,31 };
+    int numberlist [9] = { 8,3,1,6,4,7,10,14,13 };
     for ( int n=0 ; n<9 ; ++n )
     {
         bin->insert(numberlist[n]);
@@ -25,12 +23,12 @@ int main()
     cout << "Size: " << bin->getSize() << endl;
     bin->prettyPrint();
 
-    BinaryTree<int>::iterator it;
-    for(BinaryTree<int>::iterator it = bin->begin(); it != bin->end(); it++)
-    {
+    //BinaryTree<int>::iterator it;
+    //for(BinaryTree<int>::iterator it = bin->begin(); it != bin->end(); it++)
+    //{
         //cout << *it << endl;
-    }
+    //}
 
-    cout << endl;
+    //cout << endl;
     delete bin;
 }
